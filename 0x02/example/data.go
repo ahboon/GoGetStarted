e0d0b50e46b27eaac0a1ec8f6ec7718abef3224a
Cher Boon <simcherboon123@gmail.com>
2020-02-06 14:52:58 +0800

    0x02

--- 0x02/example/data.go
+++ 0x02/example/data.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-    "strconv"
-)
-
-type MyFirstStruct struct {
-    Name string `json:"name"`
-    Age int `json:"age"`
-}
-
-func GetString(name string,age string) *MyFirstStruct{
-    intAge,_ := strconv.Atoi(age)
-    var tag *MyFirstStruct
-    tag = &MyFirstStruct{}
-    tag.Name = name
-    tag.Age = intAge
-    return tag
+func GetString() string{
+    return "Hello World"
 }
--- 0x02/example/logic.go
+++ 0x02/example/logic.go
@@ -5,36 +5,8 @@ import (
     "github.com/gorilla/mux"
     "encoding/json"
 )
-
-type ErrorMessage struct {
-	Error string `json:"error"`
-}
-
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
-    if r.URL.Query()["name"] == nil {
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(ErrorMessage{Error: "name is missing."})
-		return
-    } 
-    name,_ := r.URL.Query()["name"]
-    if name[0] == "" {
-        		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(ErrorMessage{Error: "name is missing."})
-		return
-    }
-    if r.URL.Query()["age"] == nil {
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(ErrorMessage{Error: "age is missing."})
-		return
-    } 
-    age,_ := r.URL.Query()["age"]
-    if age[0] == "" {
-        		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(ErrorMessage{Error: "age is missing."})
-		return
-    }
-    output := GetString(name[0],age[0])
+    output := GetString()
     json.NewEncoder(w).Encode(output)
 }
 
